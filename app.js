7cc743fba089f71ad9a43501130e98f457e5be90
Federico Arriola <37492573+fedeemma@users.noreply.github.com>
2024-06-05 11:58:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
-/*function entradaCine(){
-const valorEntrada = 2350
-let importeIngresado = parseFloat(prompt("Ingrese el importe a abonar"))
+let pelicula = prompt(`
+1: Rápidos y Furiosos 10
+2: Creed 3
+3: La Extorción
+4: Super Marios Bross
+`)
 
-if(isNaN(importeIngresado)){
-    alert("importe incorrecto, vuelve a ingresar el importe")
-return
+switch (pelicula){
+
+    case "1":
+        console.log("Rápidos y Furiosos 10") 
+        break;
+    case "2":
+        console.log("Creed 3")
+        break;
+    case "3":
+        console.log("La Extorción")
+        break;
+    case "4":
+        console.log("Super Mario Bross")
+        break;
+    default:
+        console.log("La película seleccionada no está disponible")
+        break;
 }
 
-while (importeIngresado < valorEntrada){
-    const importeFaltante = valorEntrada - importeIngresado
-    alert("importe insuficiente, le faltan $" + importeFaltante + " " +"para completar la transacción")
-    importeIngresado = parseFloat(prompt("Ingrese un nuevo importe"))
-}
-const cambio = importeIngresado - valorEntrada
-alert("Su compra ha sido aceptada con éxito, su cambio es $" + cambio)
+function solicitarNombre(){
+
+    let nombre= prompt("ingrese su nombre")
+    console.log(`${nombre} su película ha sido seleccionada con éxito`)
 
 }
-
-entradaCine()
-
-*/
-
-const cartelera = function(pelicula, precio, sala, calidad){
-
-    this.pelicula = pelicula
-    this.precio = precio
-    this.sala = sala
-    this.calidad = calidad
-}
-
-let salaNueve = new cartelera ("Rápidos y Furiosos 10", 3500, 9, "D-BOX")
-let  salaSiete = new cartelera ("Creed 3", 3100, 7, "D-BOX")
-let salaCinco = new cartelera ("La Extorición", 2200, 5, "D-BOX")
-let salaTres = new cartelera ("Super Marios Bross", 2600, "D-BOX")
-
-let lista = [salaCinco, salaNueve, salaSiete, salaTres]
-
-function seleccionarPelicula(){
-
-    let peliculaSeleccionada = prompt("seleccione la película").trim().toUpperCase()
-
-    let resultado = lista.filter((peliculas)=> peliculas.pelicula.toUpperCase().includes(peliculaSeleccionada))
-    
-    if (resultado.length > 0){
-
-        console.table(resultado)
-    } else{
-        alert(`Por el momento la película ${peliculaSeleccionada} no se encuentra disponible`)
-    
-    }
-
-}
+solicitarNombre() 
